561fc5bef2fc8bdf7d41be383df0caf318295458
franco <franco@flydreamers.com>
2022-06-22 18:00:59 -0300

    Support home screen demo. Files.

--- psa/src/screens/Support/index.js
+++ psa/src/screens/Support/index.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { ButtonsContainer, PrincipalContainer, BodyContainer } from "./styled";
+import project_icon from "../../utils/icons/project_icon.png";
+import support_icon from "../../utils/icons/support_icon.png";
+import resources_icon from "../../utils/icons/resources_icon.png";
+import { TopBar } from "../../components/TopBar";
 import { useNavigate } from "react-router-dom";
+import { HomePresentation } from "../../components/HomePresentation";
 
 export const Support = () => {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <p>Esto es soporte</p>
-      <button onClick={() => navigate(-1)}>Volver</button>
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <PrincipalContainer>
+            <TopBar />
+        </PrincipalContainer>
+    );
 };
---
+++ psa/src/screens/Support/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { colors } from "../../utils/colors";
+
+export const PrincipalContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    align-items:center;
+    display: flex;
+    flex-direction: column;
+    font-family: "Roboto";
+    background-color: ${colors.backgroundGrey};
+`
+
+export const BodyContainer = styled.div`
+    width: 95%;
+    height: 100%;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding-top: 15px;
+    padding-left: 15px;
+    padding-right: 15px;
+    
+`
