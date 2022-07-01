009e4b9b50fbbfa38af0e10d9a27b065c89f4544
franco <franco@flydreamers.com>
2022-07-01 19:35:24 -0300

    Delete unnecessary code

--- psa/src/components/GenericForm/index.js
+++
@@ -1,9 +0,0 @@
-import Form from 'react-bootstrap/Form';
-
-export const GenericForm = ({children}) => {
-    return (
-        <Form>
-            {children}
-        </Form>
-    );
-}
--- psa/src/screens/Support/TicketForm/index.js
+++ psa/src/screens/Support/TicketForm/index.js
@@ -4,25 +4,30 @@ export const TicketForm = () => {
 
     const formInputs = () => {
         return (
-            <div>
-                <Form.Group className="mb-3" controlId="formTitle">
-                    <Form.Label>Titulo</Form.Label>
-                    <Form.Control type="text" placeholder="Titulo del ticket" />
-                </Form.Group>
+            <Form>
+                <div>
+                    <Form.Group className="mb-3" controlId="formTitle">
+                        <Form.Label>Titulo</Form.Label>
+                        <Form.Control type="text" placeholder="Titulo del ticket" />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formDescription">
-                    <Form.Label>Descripción</Form.Label>
-                    <Form.Control as={"textarea"} style={{ height: '200px' }} placeholder="Descripción del ticket" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </div>
+                    <Form.Group className="mb-3" controlId="formDescription">
+                        <Form.Label>Descripción</Form.Label>
+                        <Form.Control as={"textarea"} style={{ height: '200px' }} placeholder="Descripción del ticket" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </div>
+            </Form>
         )
     }
 
-    return ( formInputs() );
+    return (
+        <GenericForm>
+            {formInputs()}
+        </GenericForm>);
 }
