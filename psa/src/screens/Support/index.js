846a8f40fdb006bb4ec7b869391b889af75a31d9
franco <franco@flydreamers.com>
2022-07-01 16:35:20 -0300

    navigates

--- psa/src/screens/Support/index.js
+++ psa/src/screens/Support/index.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { ButtonIcon } from "../../components/ButtonIcon";
-import { ButtonsContainer, PrincipalContainer, BodyContainer } from "./styled";
-import project_icon from "../../utils/icons/project_icon.png";
-import support_icon from "../../utils/icons/support_icon.png";
-import resources_icon from "../../utils/icons/resources_icon.png";
+import { PrincipalContainer, BodyContainer } from "./styled";
 import { TopBar } from "../../components/TopBar";
 import { useNavigate } from "react-router-dom";
-import { HomePresentation } from "../../components/HomePresentation";
-
+import ListCards from "../../components/ListCards/ListCards";
 export const Support = () => {
     const navigate = useNavigate();
     return (
         <PrincipalContainer>
-            <TopBar />
+            <TopBar buttonSelected ={"Soporte"} />
+            <ListCards />
+
         </PrincipalContainer>
     );
 };
