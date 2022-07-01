c3bbf29b6280cab8c591de259098285bae6bd42a
franco <franco@flydreamers.com>
2022-07-01 16:59:18 -0300

    wip

--- psa/src/screens/Support/index.js
+++ psa/src/screens/Support/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import { PrincipalContainer, BodyContainer } from "./styled";
 import { TopBar } from "../../components/TopBar";
 import { useNavigate } from "react-router-dom";
-import ListCards from "../../components/ListCards/ListCards";
 export const Support = () => {
     const navigate = useNavigate();
     return (
         <PrincipalContainer>
-            <TopBar buttonSelected ={"Soporte"} />
-            <ListCards />
-
+            <TopBar buttonSelected = {"Soporte"} />
         </PrincipalContainer>
     );
 };
