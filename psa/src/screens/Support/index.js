344728798bf370b6f61376f19931229d2d05bb37
franco <franco@flydreamers.com>
2022-07-01 19:16:03 -0300

    Create example of generic form and modal

--- psa/src/screens/Support/index.js
+++ psa/src/screens/Support/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import {GenericModal} from "../../components/GenericModal";
+
+import {Button} from "react-bootstrap";
+import {TicketForm} from "./TicketForm";
 
 export const Support = () => {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <p>Esto es soporte</p>
-      <button onClick={() => navigate(-1)}>Volver</button>
+    const [modalShow, setModalShow] = useState(false);
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <p>Esto es soporte</p>
+            <button onClick={() => navigate("/")}>Volver</button>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+                Launch vertically centered modal
+            </Button>
+
+            <GenericModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                form = {<TicketForm/>}
+                title = {"Crear ticket"}
+            />
     </div>
-  );
+    );
 };
