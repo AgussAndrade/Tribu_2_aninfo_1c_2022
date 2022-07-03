420ed2754546e12f8802f26a2cf7ecb4c4fece7a
Agustina Bocaccio <73569719+agusbocaccio@users.noreply.github.com>
2022-07-03 10:08:21 -0300

    error page

--- psa/src/screens/ErrorPage/index.js
+++ psa/src/screens/ErrorPage/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { PrincipalContainer } from "../Project/styled";
-import { ErrorMessageContainer } from "./styled";
+import { ErrorMessage, ErrorMessageContainer } from "./styled";
 
 export const ErrorPage = () => { 
     return(<ErrorMessageContainer>
-
+        <ErrorMessage>
+            Lo sentimos, ocurrió un error. Intente de nuevo en unos minutos
+        </ErrorMessage>
     </ErrorMessageContainer>)
 }
