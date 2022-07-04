192f502334e8edf3b699b32efc794fbf731d7ee5
franco <franco@flydreamers.com>
2022-07-03 22:42:02 -0300

    wip

--- psa/src/screens/Support/components/TicketCreateForm/index.js
+++ psa/src/screens/Support/components/TicketCreateForm/index.js
@@ -1,31 +1,10 @@
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 import {useState} from "react";
-import {useFormik} from "formik";
-import * as Yup from 'yup';
 
 
 export const TicketCreateForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            title: '',
-            description: '',
-            severity: '',
-            responsible: '',
-            id_client: '',
-        },
-        validationSchema: Yup.object({
-            firstName: Yup.string()
-                .max(15, 'Must be 15 characters or less')
-                .required('Required'),
-            lastName: Yup.string()
-                .max(20, 'Must be 20 characters or less')
-                .required('Required'),
-            email: Yup.string().email('Invalid email address').required('Required'),
-        }),
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
+    const [validated, setValidated] = useState(false);
+
     const handleSubmit = (event) => {
         const form = event.currentTarget;
         event.preventDefault();
@@ -33,7 +12,7 @@ export const TicketCreateForm = () => {
         if (form.checkValidity() === false) {
             event.stopPropagation();
         }
-        setValidated(true);
+        setValidated(true)
     };
 
     const formInputs = () => {
@@ -48,7 +27,6 @@ export const TicketCreateForm = () => {
                                     type="text"
                                     placeholder="Titulo del ticket"
                                     name = "title"
-                                    value={title}
                                     required
                                 />
                                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
@@ -56,12 +34,12 @@ export const TicketCreateForm = () => {
 
                             <Form.Group className="mb-3" controlId="description">
                                 <Form.Label>Descripción</Form.Label>
-                                <Form.Control as={"textarea"} style={{ height: '200px', resize: "none" }} placeholder="Descripción del ticket" value={description}/>
+                                <Form.Control as={"textarea"} style={{ height: '200px', resize: "none" }} placeholder="Descripción del ticket" />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId= "severity" >
                                 <Form.Label>Severidad</Form.Label>
-                                <Form.Select value={severity}>
+                                <Form.Select>
                                     <option value="3">Mayor</option>
                                     <option value="2">Medio</option>
                                     <option value="1">Baja</option>
@@ -71,7 +49,7 @@ export const TicketCreateForm = () => {
                         <Col>
                             <Form.Group className="mb-3" controlId= "responsible" >
                                 <Form.Label>Responsable</Form.Label>
-                                <Form.Control as={"input"} list = "employers" value={responsible}></Form.Control>
+                                <Form.Control as={"input"} list = "employers" ></Form.Control>
                                 <datalist id={"employers"}>
                                     <option value="Julian" data-id-employer = "3"></option>
                                     <option value="Juan" data-id-employer = "2"></option>
@@ -80,12 +58,12 @@ export const TicketCreateForm = () => {
                             </Form.Group>
                             <Form.Group className="mb-3" controlId= "end_time" >
                                 <Form.Label>Fecha de vencimiento</Form.Label>
-                                <Form.Control type="date" name='end_time' value={endDate}/>
+                                <Form.Control type="date" name='end_time'/>
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId= "client_id" >
                                 <Form.Label>Cliente</Form.Label>
-                                <Form.Control as={"input"} list ="clientes" name ="id_client" value={clientId}></Form.Control>
+                                <Form.Control as={"input"} list ="clientes" name ="id_client"></Form.Control>
                                 <datalist id={"clientes"}>
                                     <option value="Julian" data-id-client = "3"></option>
                                     <option value="Juan" data-id-client = "2"></option>
@@ -94,7 +72,7 @@ export const TicketCreateForm = () => {
                             </Form.Group>
                         </Col>
                     </Row>
-                    <div style={{display: 'flex', "justify-content": 'flex-end'}}>
+                    <div style={{display: 'flex', justifyContent: 'flex-end'}}>
                         <Button variant="primary" type="submit">
                             Guardar
                         </Button>
